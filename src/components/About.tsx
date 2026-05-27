7a922565413c1c1c17f984d61de96ed6394d7636
Abdullah Mehmood <abdullahmehmood704@gmail.com>
2026-05-27 21:14:39 +0500

    Fix typescript error in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -39,7 +39,7 @@ const container = {
 
 const item = {
     hidden: { opacity: 0, y: 40 },
-    show: { opacity: 1, y: 0, transition: { duration: 0.55, ease: [0.25, 0.46, 0.45, 0.94] } },
+    show: { opacity: 1, y: 0, transition: { duration: 0.55, ease: 'easeOut' } },
 }
 
 export default function About() {
